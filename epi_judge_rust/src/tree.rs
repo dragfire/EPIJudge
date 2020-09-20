66a7ed3f6878edf2bd54faa2550d85cdd804d16b
dragfire <asem.devajit@gmail.com>
2020-09-20 13:44:00 -0600

    Fix build_tree, print_inorder

--- epi_judge_rust/src/tree.rs
+++ epi_judge_rust/src/tree.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::fmt::Debug;
@@ -24,7 +23,7 @@ where
     }
 }
 
-struct Tree<T>(Option<TreeNode<T>>);
+struct Tree<T>(Option<Rc<RefCell<TreeNode<T>>>>);
 
 impl<T> Tree<T>
 where
@@ -34,58 +33,59 @@ where
         Tree(None)
     }
 
-    fn build_tree(&mut self, data: Vec<Option<T>>) {
-        let mut nodes: Vec<Option<RefCell<TreeNode<T>>>> = data
+    pub fn build_tree(&mut self, data: Vec<Option<T>>) {
+        let mut nodes: Vec<Option<Rc<RefCell<TreeNode<T>>>>> = data
             .into_iter()
-            .map(|d| d.map(TreeNode::new).map(RefCell::new))
+            .map(|d| d.map(TreeNode::new).map(RefCell::new).map(Rc::new))
             .collect::<Vec<_>>();
 
-        let mut candidate_children: VecDeque<&Option<RefCell<TreeNode<T>>>> =
-            nodes.iter().collect::<VecDeque<_>>();
+        let mut candidate_children: VecDeque<Option<Rc<RefCell<TreeNode<T>>>>> =
+            VecDeque::from(nodes.clone());
 
         candidate_children.pop_front();
 
         for tnode in nodes.iter() {
+            if candidate_children.is_empty() {
+                break;
+            }
             if let Some(node) = tnode {
                 if let Some(n) = candidate_children.pop_front() {
                     if let Some(ref n) = n {
-                        node.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(
-                            n.borrow_mut().data.clone(),
-                        ))));
+                        node.borrow_mut().left = Some(n.clone());
                     }
                 }
                 if let Some(n) = candidate_children.pop_front() {
                     if let Some(ref n) = n {
-                        node.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(
-                            n.borrow_mut().data.clone(),
-                        ))));
+                        node.borrow_mut().right = Some(n.clone());
                     }
                 }
             }
         }
 
-        self.0 = Some(nodes.swap_remove(0).unwrap().into_inner());
+        self.0 = nodes.swap_remove(0);
     }
 
-    fn print_inorder(&self) {
-        let mut queue = VecDeque::new();
-        queue.push_front(self.0.as_ref());
-        println!("{:?}", self.0.as_ref());
+    pub fn print_inorder(&self) {
+        let curr = self.0.clone();
+        Tree::print_inorder_helper(curr);
+    }
+
+    fn print_inorder_helper(curr: Option<Rc<RefCell<TreeNode<T>>>>) {
+        match curr {
+            Some(ref node) => {
+                let cloned_node = node.clone();
+                Tree::print_inorder_helper(cloned_node.borrow().left.clone());
+                print!("{:?} ", node.borrow().data);
+                Tree::print_inorder_helper(cloned_node.borrow().right.clone());
+            }
+            None => return,
+        }
     }
 }
 
 #[test]
 fn test_tree() {
-    let v = vec![
-        Some(1),
-        None,
-        Some(2),
-        Some(3),
-        Some(4),
-        Some(5),
-        None,
-        Some(6),
-    ];
+    let v = vec![Some(1), Some(2), Some(3), Some(4), Some(5), None, Some(6)];
     let mut tree = Tree::new();
     tree.build_tree(v);
     tree.print_inorder();
